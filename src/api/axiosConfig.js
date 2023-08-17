2e8718e03eefa830ba682fe5a6b82de6ad6f9eb6
02_bao <104081842+02bao@users.noreply.github.com>
2023-08-17 21:03:02 +0700

    load data from backend

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'http://192.168.1.5:8080',
+    headers: {"ngrok-skip-browser-warning": "true"}
+
+});
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,40 @@
+import './Hero.css';
+import Carousel from 'react-material-ui-carousel'; 
+import { Paper } from '@mui/material';
+// bây giờ mình ẩn vài dòng coi thử lỗi có bị mất ko hen
+//ctrl + /   :comment//uncomment
+const Hero = ({movies}) => {
+    return (
+        <div className='movie-carousel-cpntainer'>
+              <Carousel> 
+                {
+                    movies.map((movie) => {
+                        return (
+                            <Paper>
+                                <div className='movie-card-cpntainer'>
+                                    <div className="movie-card">
+                                        <div className="movie-detail">
+                                            <div className="movie-poster">
+                                                <img src = {movie.poster} alt = "" />
+                                            </div>
+                                            <div className="movie-title">
+                                                <h4>{movie.title} </h4>
+                                             </div>
+                                        </div>
+                                    </div>
+                                </div>
+                               
+                            </Paper>
+                        )
+                    })
+                }
+          
+                        
+            </Carousel>
+        </div>
+    )
+}
+Hero.defaultProps = {
+    movies: []
+};  
+export default Hero
