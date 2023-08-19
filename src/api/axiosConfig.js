0215b2be3146e6d8b9d0ec343f2bcc2e3874dc6e
02_bao <104081842+02bao@users.noreply.github.com>
2023-08-19 20:27:29 +0700

    bug in 2:14:47 to 2:19:30

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://192.168.1.5:8080',
+    baseURL:'http://192.168.1.6:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 
 });
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap/Button";
+import Continer from "react-bootstrap/Continer";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
 
 const Header () => {
-    return (
-        <div>
 
-        </div>
-    )
+return (
+
+        <Navbar bg = "dark" variant= "dark" expand="lg">
+            <Continer fluid>
+
+                <Navbar.Brand href="/" style={{"color": 'gold'}} >
+
+                    <FontAwesomeIcon icon = {faVieoSlash}/> Gold
+                
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse if="navbarScroll">
+                
+                    <Nav
+                        className = "me-auto my-2 my-lg-0"
+                        style={{maxHeight: '100px'}}
+                        navbarScroll
+                    >
+                        <NavLink className="nav-link" to="/">Home</NavLink>  
+                        <NavLink className="nav-link" to="/watchlist">Home</NavLink>   
+                    </Nav>
+                    <Button variant="outline-info" className="me-2" Login></Button>
+                    <Button variant="outline-info"  Register></Button>
+                </Navbar.Collapse>
+
+            </Continer>
+
+        </Navbar>
+  )
 }
 
 export default Header 
