0faec1b5d0225e8d692aac05279600fdedae9a7d
02_bao <104081842+02bao@users.noreply.github.com>
2023-08-28 22:28:50 +0700

    bug in 2:20:00

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://192.168.1.6:8080',
+    baseURL:'http://192.168.1.9:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 
 });
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,14 +4,14 @@ import { Paper } from '@mui/material';
 
 const Hero = ({movies}) => {
     return (
-        <div className='movie-carousel-cpntainer'>
+        <div className='movie-carousel-container'>
               <Carousel> 
                 {
                     movies.map((movie) => {
                         return (
                             <Paper>
-                                <div className='movie-card-cpntainer'>
-                                    <div className="movie-card" style ={{"--img": 'url(${movie.backdrops[0]})'}} >
+                                <div className='movie-card-container'>
+                                    <div className="movie-card" style ={{"--img": `url(${movie.backdrops[0]})`}} >
                                         <div className="movie-detail">
                                             <div className="movie-poster">
                                                 <img src = {movie.poster} alt = "" />
